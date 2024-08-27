f034be444224001ad9a7513d1552ec8e97e7cd52
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-08-27 17:17:37 -0300

    fix: sorting by number of reactions

--- src/components/questions.tsx
+++ src/components/questions.tsx
@@ -1,14 +1,13 @@
 import { useParams } from "react-router-dom";
-import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
-import { getRoomQuestions, GetRoomQuestionsResponse } from "../http/get-room-questions";
+import { getRoomQuestions } from "../http/get-room-questions";
+
+import { useQuestionsWebSockets } from "../hooks/create-questions-websocket";
 
 import { Question } from "./question";
-import { useEffect } from "react";
 
 export function Questions() {
-  // Hooks
-  const queryClient = useQueryClient();
   const { roomId } = useParams();
 
   if (!roomId) throw new Error('QuestionList component must be used inside a Room component')
@@ -18,88 +17,16 @@ export function Questions() {
     queryFn: () => getRoomQuestions({ roomId }),
   })
 
-  // Constants
-  const { questions } = data;
+  useQuestionsWebSockets({ roomId })
 
-  // Effects
-  useEffect(() => {
-    const ws = new WebSocket(`ws://localhost:8080/subscribe/${roomId}`);
-
-    ws.onopen = () => {
-      console.log('Websocket connected!')
-    }
-
-    ws.onclose = () => {
-      console.log('Websocket connection closed!')
-    }
-
-    ws.onmessage = (event) => {
-      const data: {
-        kind: 'message_created' | 'message_answered' | 'message_reaction_increased' | 'message_reaction_decreased',
-        value: any
-      } = JSON.parse(event.data);
-
-      switch(data.kind) {
-        case 'message_created':
-          queryClient.setQueryData<GetRoomQuestionsResponse>(['questions', roomId], state => {
-            return {
-              questions: [
-                ...(state?.questions ?? []),
-                {
-                  id: data.value.id,
-                  text: data.value.message,
-                  amountOfReactions: 0,
-                  answered: false
-                }
-              ],
-            }
-          })
-        break;
-
-        case 'message_answered':
-          queryClient.setQueryData<GetRoomQuestionsResponse>(['questions', roomId], state => {
-            if (!state) return undefined;
-
-            return {
-              questions: state.questions.map(question => {
-                if (question.id === data.value.id) {
-                  return { ...question, answered: true }
-                }
-
-                return question
-              })
-            }
-          })
-        break;
-
-        case 'message_reaction_decreased':
-        case 'message_reaction_increased':
-          queryClient.setQueryData<GetRoomQuestionsResponse>(['questions', roomId], state => {
-            if (!state) return undefined;
-
-            return {
-              questions: state.questions.map(question => {
-                if (question.id === data.value.id) {
-                  return { ...question, amountOfReactions: data.value.count }
-                }
-
-                return question
-              })
-            }
-          })
-        break;
-      }
-    }
-
-    return () => {
-      ws.close()
-    }
-  }, [roomId, queryClient])
+  const sortedQuestons = data.questions.sort((a,b) => {
+    return b.amountOfReactions - a.amountOfReactions
+  })
 
   // Renders
   return (
     <ol className='list-decimal list-outside px-3 space-y-8'>
-      {questions.map(question => (
+      {sortedQuestons.map(question => (
         <Question
           key={question.id}
           questionId={question.id}
---
+++ src/hooks/create-questions-websocket.ts
@@ -0,0 +1,95 @@
+import { useEffect } from "react";
+
+import { useQueryClient } from "@tanstack/react-query";
+import { GetRoomQuestionsResponse } from "../http/get-room-questions";
+
+interface UseQuestionsWebsocketsParams {
+  roomId: string
+}
+
+type WebhookMessage =
+  | {kind: "message_created", value: { id: string, message: string }}
+  | {kind: "message_answered", value: { id: string }}
+  | {kind: "message_reaction_increased", value: { id: string, count: number }}
+  | {kind: "message_reaction_decreased", value: { id: string, count: number }}
+
+
+
+export function useQuestionsWebSockets ({
+  roomId
+}: UseQuestionsWebsocketsParams) {
+  // Hooks
+  const queryClient = useQueryClient()
+
+  // Effects
+  useEffect(() => {
+    const ws = new WebSocket(`ws://localhost:8080/subscribe/${roomId}`);
+
+    ws.onopen = () => {
+      console.log('Websocket connected!')
+    }
+
+    ws.onclose = () => {
+      console.log('Websocket connection closed!')
+    }
+
+    ws.onmessage = (event) => {
+      const data: WebhookMessage = JSON.parse(event.data);
+
+      switch(data.kind) {
+        case 'message_created':
+          queryClient.setQueryData<GetRoomQuestionsResponse>(['questions', roomId], state => {
+            return {
+              questions: [
+                ...(state?.questions ?? []),
+                {
+                  id: data.value.id,
+                  text: data.value.message,
+                  amountOfReactions: 0,
+                  answered: false
+                }
+              ],
+            }
+          })
+        break;
+
+        case 'message_answered':
+          queryClient.setQueryData<GetRoomQuestionsResponse>(['questions', roomId], state => {
+            if (!state) return undefined;
+
+            return {
+              questions: state.questions.map(question => {
+                if (question.id === data.value.id) {
+                  return { ...question, answered: true }
+                }
+
+                return question
+              })
+            }
+          })
+        break;
+
+        case 'message_reaction_decreased':
+        case 'message_reaction_increased':
+          queryClient.setQueryData<GetRoomQuestionsResponse>(['questions', roomId], state => {
+            if (!state) return undefined;
+
+            return {
+              questions: state.questions.map(question => {
+                if (question.id === data.value.id) {
+                  return { ...question, amountOfReactions: data.value.count }
+                }
+
+                return question
+              })
+            }
+          })
+        break;
+      }
+    }
+
+    return () => {
+      ws.close()
+    }
+  }, [roomId, queryClient])
+}
